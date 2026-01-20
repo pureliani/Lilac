3ad7d77d927d56af89589b08758131ffa47a25f2
Gabriel Pureliani <gafureliani@gmail.com>
2026-01-20 16:37:00 +0400

    refactor: migrate the if expr/stmt builder

--- src/hir/expressions/if.rs
+++ src/hir/expressions/if.rs
@@ -1,16 +1,17 @@
+use std::collections::HashMap;
+
 use crate::{
     ast::{
         expr::{BlockContents, Expr},
         Span,
     },
     hir::{
-        cfg::{BasicBlockId, Terminator, Value},
+        builders::{BasicBlockId, Builder, InBlock, ValueId},
         errors::{SemanticError, SemanticErrorKind},
         types::checked_type::Type,
         utils::{
             check_is_assignable::check_is_assignable, try_unify_types::try_unify_types,
         },
-        FunctionBuilder, HIRContext,
     },
 };
 
@@ -21,138 +22,131 @@ pub enum IfContext {
     /// The `if` is used for control flow, its value is discarded
     Statement,
 }
-// src/hir/expressions/if.rs
 
-impl FunctionBuilder {
+impl<'a> Builder<'a, InBlock> {
     pub fn build_if(
         &mut self,
-        ctx: &mut HIRContext,
         branches: Vec<(Box<Expr>, BlockContents)>,
         else_branch: Option<BlockContents>,
         context: IfContext,
-    ) -> Value {
+    ) -> ValueId {
         if context == IfContext::Expression && else_branch.is_none() {
-            let span = branches.first().unwrap().0.span;
-            return Value::Use(self.report_error_and_get_poison(
-                ctx,
-                SemanticError {
-                    kind: SemanticErrorKind::IfExpressionMissingElse,
-                    span,
-                },
-            ));
+            let span = branches.first().unwrap().0.span.clone();
+            return self.report_error_and_get_poison(SemanticError {
+                kind: SemanticErrorKind::IfExpressionMissingElse,
+                span,
+            });
         }
 
-        let merge_block_id = self.new_basic_block();
-        let mut branch_results: Vec<(BasicBlockId, Value, Span)> = Vec::new();
-        let mut last_condition_block_id = self.current_block_id;
+        let merge_block_id = self.as_fn().new_bb();
+        let mut branch_results: Vec<(BasicBlockId, ValueId, Span)> = Vec::new();
+        let mut current_condition_block_id = self.context.block_id;
+
+        let get_final_expr_span = |block: &BlockContents| {
+            block
+                .final_expr
+                .as_ref()
+                .map(|f| &f.span)
+                .unwrap_or(&block.span)
+                .clone()
+        };
 
         for (condition, body) in branches {
-            let condition_span = condition.span;
-            let body_span = body.span;
+            self.use_basic_block(current_condition_block_id);
 
-            self.use_basic_block(last_condition_block_id);
+            let condition_span = condition.span.clone();
 
-            let condition_value = self.build_expr(ctx, *condition);
-            let condition_value_type =
-                ctx.program_builder.get_value_type(&condition_value);
+            let cond_id = self.build_expr(*condition);
+            let cond_ty = self.get_value_type(&cond_id);
 
-            if !check_is_assignable(&condition_value_type, &Type::Bool) {
-                ctx.module_builder.errors.push(SemanticError {
+            if !check_is_assignable(cond_ty, &Type::Bool) {
+                self.errors.push(SemanticError {
                     span: condition_span,
                     kind: SemanticErrorKind::TypeMismatch {
                         expected: Type::Bool,
-                        received: condition_value_type,
+                        received: cond_ty.clone(),
                     },
                 });
             }
 
-            let body_block_id = self.new_basic_block();
-            let next_condition_block_id = self.new_basic_block();
+            let then_block_id = self.as_fn().new_bb();
+            let next_cond_block_id = self.as_fn().new_bb();
 
-            if let Value::Use(cond_id) = condition_value {
-                if let Some(pred) = self.predicates.get(&cond_id).cloned() {
-                    self.map_value(body_block_id, pred.source, pred.true_id);
-                    self.map_value(next_condition_block_id, pred.source, pred.false_id);
-                }
+            if let Some(pred) = self.get_fn().predicates.get(&cond_id).cloned() {
+                self.get_bb_mut(then_block_id)
+                    .original_to_local_valueid
+                    .insert(pred.source, pred.true_id);
+
+                self.get_bb_mut(next_cond_block_id)
+                    .original_to_local_valueid
+                    .insert(pred.source, pred.false_id);
             }
 
-            self.set_basic_block_terminator(Terminator::CondJump {
-                condition: condition_value,
-                true_target: body_block_id,
-                true_args: vec![],
-                false_target: next_condition_block_id,
-                false_args: vec![],
-            });
+            self.cond_jmp(
+                cond_id,
+                then_block_id,
+                HashMap::new(),
+                next_cond_block_id,
+                HashMap::new(),
+            );
 
-            self.seal_block(ctx, body_block_id);
+            self.seal_block(then_block_id);
+            self.use_basic_block(then_block_id);
+            let final_expr_span = get_final_expr_span(&body);
+            let then_val = self.build_codeblock_expr(body);
 
-            self.use_basic_block(body_block_id);
-            let body_value = self.build_codeblock_expr(ctx, body);
-
-            if self.get_current_basic_block().terminator.is_none() {
-                branch_results.push((self.current_block_id, body_value, body_span));
+            if self.bb().terminator.is_none() {
+                branch_results.push((self.context.block_id, then_val, final_expr_span));
             }
 
-            self.seal_block(ctx, next_condition_block_id);
-            last_condition_block_id = next_condition_block_id;
+            current_condition_block_id = next_cond_block_id;
         }
 
-        self.use_basic_block(last_condition_block_id);
-
+        self.use_basic_block(current_condition_block_id);
         if let Some(else_body) = else_branch {
-            let else_span = else_body.span;
-            let else_value = self.build_codeblock_expr(ctx, else_body);
+            let final_expr_span = get_final_expr_span(&else_body);
+            let else_val = self.build_codeblock_expr(else_body);
 
-            if self.get_current_basic_block().terminator.is_none() {
-                branch_results.push((self.current_block_id, else_value, else_span));
+            if self.bb().terminator.is_none() {
+                branch_results.push((self.context.block_id, else_val, final_expr_span));
             }
-        } else if context == IfContext::Statement {
-            self.set_basic_block_terminator(Terminator::Jump {
-                target: merge_block_id,
-                args: vec![],
-            });
+        } else {
+            self.jmp(merge_block_id, HashMap::new());
         }
+
+        self.seal_block(current_condition_block_id);
 
         let result_param_id = if context == IfContext::Expression {
             let type_entries: Vec<(Type, Span)> = branch_results
                 .iter()
-                .map(|(_, val, span)| (ctx.program_builder.get_value_type(val), *span))
+                .map(|(_, val, span)| (self.get_value_type(val).clone(), span.clone()))
                 .collect();
 
             let result_type = match try_unify_types(&type_entries) {
                 Ok(ty) => ty,
                 Err(e) => {
-                    ctx.module_builder.errors.push(e);
+                    self.errors.push(e);
                     Type::Unknown
                 }
             };
 
-            Some(self.append_block_param(ctx, merge_block_id, result_type))
+            Some(self.append_param_to_block(merge_block_id, result_type))
         } else {
             None
         };
 
         for (block_id, val, _) in branch_results {
             self.use_basic_block(block_id);
-            let args = if result_param_id.is_some() {
-                vec![val]
-            } else {
-                vec![]
-            };
-
-            self.set_basic_block_terminator(Terminator::Jump {
-                target: merge_block_id,
-                args,
-            });
+            let mut args = HashMap::new();
+            if let Some(param_id) = result_param_id {
+                args.insert(param_id, val);
+            }
+            self.jmp(merge_block_id, args);
         }
 
-        self.seal_block(ctx, merge_block_id);
+        self.seal_block(merge_block_id);
         self.use_basic_block(merge_block_id);
 
-        if let Some(pid) = result_param_id {
-            Value::Use(pid)
-        } else {
-            Value::VoidLiteral
-        }
+        result_param_id.unwrap_or_else(|| self.emit_const_void())
     }
 }
