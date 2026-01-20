491a291285cab4f835be3a70a66c96150332a090
Gabriel Pureliani <gafureliani@gmail.com>
2026-01-20 14:07:29 +0400

    refactor: migrate tag expressions

--- src/hir/expressions/tag.rs
+++ src/hir/expressions/tag.rs
@@ -1,76 +1,61 @@
 use crate::{
     ast::{expr::Expr, IdentifierNode, Span},
+    globals::{COMMON_IDENTIFIERS, TAG_INTERNER},
     hir::{
-        cfg::Value,
+        builders::{Builder, InBlock, ValueId},
         types::{
             checked_declaration::TagType,
             checked_type::{StructKind, Type},
         },
-        FunctionBuilder, HIRContext,
     },
     tokenize::NumberKind,
+    unwrap_or_poison,
 };
 
-impl FunctionBuilder {
+impl<'a> Builder<'a, InBlock> {
     pub fn build_tag_expr(
         &mut self,
-        ctx: &mut HIRContext,
         name: IdentifierNode,
         value: Option<Box<Expr>>,
         span: Span,
-    ) -> Value {
-        let identifier_value = COMMON_IDENTIFIERS.value;
-        let identifier_id = COMMON_IDENTIFIERS.id;
+    ) -> ValueId {
+        let tag_id = TAG_INTERNER.intern(&name.name);
 
-        let tag_id = ctx.program_builder.tag_interner.intern(&name.name);
-
-        let inner_value = value.map(|v| {
-            let value_span = v.span;
-            (Box::new(self.build_expr(ctx, *v)), value_span)
+        let inner_info = value.map(|v| {
+            let v_span = v.span.clone();
+            let v_id = self.build_expr(*v);
+            let v_ty = self.get_value_type(&v_id).clone();
+            (v_id, v_ty, v_span)
         });
 
-        let inner_value_type = inner_value
-            .as_ref()
-            .map(|(value, _)| Box::new(ctx.program_builder.get_value_type(value)));
-
         let checked_type = Type::Struct(StructKind::Tag(TagType {
-            span,
             id: tag_id,
-            value_type: inner_value_type,
+            value_type: inner_info.as_ref().map(|(_, ty, _)| Box::new(ty.clone())),
+            span: span.clone(),
         }));
 
-        let tag_ptr = self.emit_stack_alloc(ctx, checked_type.clone(), 1);
+        let tag_ptr = self.emit_stack_alloc(checked_type.clone(), 1);
 
         let id_field_node = IdentifierNode {
-            name: identifier_id,
-            span,
+            name: COMMON_IDENTIFIERS.id,
+            span: span.clone(),
         };
 
-        let id_ptr = self
-            .emit_get_field_ptr(ctx, tag_ptr, id_field_node)
-            .expect("INTERNAL COMPILER ERROR: StructKind::Tag missing 'id' field");
+        let id_ptr = unwrap_or_poison!(self, self.get_field_ptr(tag_ptr, &id_field_node));
+        let tag_id_val = self.emit_const_number(NumberKind::U16(tag_id.0));
+        self.store(id_ptr, tag_id_val, span.clone());
 
-        self.emit_store(
-            ctx,
-            id_ptr,
-            Value::NumberLiteral(NumberKind::U16(tag_id.0)),
-            Span::default(),
-        );
-
-        if let Some((v, v_span)) = inner_value {
+        if let Some((v_id, _, v_span)) = inner_info {
             let val_field_node = IdentifierNode {
-                name: identifier_value,
-                span,
+                name: COMMON_IDENTIFIERS.value,
+                span: span.clone(),
             };
 
-            let value_ptr = self
-                .emit_get_field_ptr(ctx, tag_ptr, val_field_node)
-                .expect("INTERNAL COMPILER ERROR: StructKind::Tag missing 'value' field");
-
-            self.emit_store(ctx, value_ptr, *v, v_span);
+            let value_ptr =
+                unwrap_or_poison!(self, self.get_field_ptr(tag_ptr, &val_field_node));
+            self.store(value_ptr, v_id, v_span);
         }
 
-        let final_val = self.emit_load(ctx, tag_ptr);
-        Value::Use(final_val)
+        unwrap_or_poison!(self, self.load(tag_ptr, span))
     }
 }
