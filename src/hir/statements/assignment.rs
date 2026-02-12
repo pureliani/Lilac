d90310a0415106447f8ba3aec5d8737682748d20
Gabriel Pureliani <gafureliani@gmail.com>
2026-02-13 01:07:15 +0400

    fix: assignment statements

--- src/hir/statements/assignment.rs
+++ src/hir/statements/assignment.rs
@@ -3,7 +3,6 @@ use crate::{
     hir::{
         builders::{Builder, InBlock},
         errors::{SemanticError, SemanticErrorKind},
-        types::checked_type::Type,
         utils::{adjustments::Adjustment, place::Place},
     },
 };
@@ -15,13 +14,40 @@ impl<'a> Builder<'a, InBlock> {
         value: Expr,
     ) -> Result<(), SemanticError> {
         let value_span = value.span.clone();
+        let target_span = target.span.clone();
         let value_id = self.build_expr(value)?;
 
-        let target_span = target.span.clone();
         let place = self.resolve_place(target)?;
-        let place_path = place.path();
-        let place_type = self.type_of_place(&place);
 
-        todo!();
+        if matches!(&place, Place::Temporary(_)) {
+            return Err(SemanticError {
+                kind: SemanticErrorKind::InvalidLValue,
+                span: target_span,
+            });
+        }
+
+        let constraint_type = self.type_of_place(&place);
+
+        let adjustment = self
+            .compute_adjustment(value_id, &constraint_type, false)
+            .map_err(|kind| SemanticError {
+                kind,
+                span: value_span.clone(),
+            })?;
+
+        let stored_value = if matches!(
+            &adjustment,
+            Adjustment::Identity
+                | Adjustment::WrapInUnion { .. }
+                | Adjustment::WidenUnion { .. }
+        ) {
+            value_id
+        } else {
+            self.apply_adjustment(value_id, adjustment, value_span)
+        };
+
+        self.remap_place(&place, stored_value);
+
+        Ok(())
     }
 }
