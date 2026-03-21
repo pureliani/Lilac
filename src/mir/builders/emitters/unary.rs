7f1e498edb1095512d5c7235e2b9d2c0da94366b
Gabriel Pureliani <gafureliani@gmail.com>
2026-03-21 12:40:26 +0400

    refactor: unary.rs to use the new type interner

--- src/mir/builders/emitters/unary.rs
+++ src/mir/builders/emitters/unary.rs
@@ -1,8 +1,6 @@
 use crate::mir::{
     builders::{Builder, InBlock, TypePredicate, ValueId},
     instructions::{Instruction, UnaryInstr},
-    types::checked_type::Type,
-    utils::numeric::{is_float, is_signed},
 };
 
 impl<'a> Builder<'a, InBlock> {
@@ -21,26 +19,27 @@ impl<'a> Builder<'a, InBlock> {
     }
 }
 impl<'a> Builder<'a, InBlock> {
-    pub fn neg(&mut self, src: ValueId) -> ValueId {
-        let ty = self.get_value_type(src);
+    pub fn neg(&mut self, value: ValueId) -> ValueId {
+        let value_type = self.get_value_type(value);
 
-        if is_float(ty) {
-            self.emit_fneg(src)
-        } else if is_signed(ty) {
-            self.emit_ineg(src)
+        if self.types.is_float(value_type) {
+            self.emit_fneg(value)
+        } else if self.types.is_signed(value_type) {
+            self.emit_ineg(value)
         } else {
             panic!("INTERNAL COMPILER ERROR: Cannot apply unary negation `-` operator to this type")
         }
     }
 
     pub fn not(&mut self, src: ValueId) -> ValueId {
-        let ty = self.get_value_type(src);
+        let value_type = self.get_value_type(src);
 
-        if !matches!(ty, &Type::Bool(_)) {
-            panic!("INTERNAL COMPILER ERROR: Cannot apply unary not `-` operator to this type")
+        if !self.types.is_bool(value_type) {
+            panic!("INTERNAL COMPILER ERROR: Cannot apply unary not `!` operator to this type")
         }
 
-        let dest = self.new_value_id(Type::Bool(None));
+        let bool_type = self.types.bool(None);
+        let dest = self.new_value_id(bool_type);
 
         if let Some(preds) = self.type_predicates.get(&src).cloned() {
             let flipped: Vec<TypePredicate> = preds
